a8188b33930cae343ffd2494f600552da6cfbe8e
Helen Williamson <1506259+helenwilliamson@users.noreply.github.com>
2022-12-02 18:20:05 +0000

    day 2 - part 2 + some refactoring

--- src/advent_of_code_2022/puzzle-2.clj
+++ src/advent_of_code_2022/puzzle-2.clj
@@ -1,32 +1,63 @@
 (ns advent-of-code-2022.puzzle-2
   (:gen-class)
-  (:require [clojure.string :as str]))
+  (:require [clojure.string :as str])
+  (:require [clojure.set :as set]))
 
-(def conversion
-  {"A" :rock, "B" :paper, "C" :scissors, "X" :rock, "Y" :paper, "Z" :scissors})
+(def conversion-move {"A" :rock
+                      "B" :paper
+                      "C" :scissors
+                      "X" :rock
+                      "Y" :paper
+                      "Z" :scissors})
 
-(def score-for-play
-  {:rock 1, :paper 2, :scissors 3})
+(def conversion-outcome {"X" :lose
+                         "Y" :draw,
+                         "Z" :win})
+
+(def winning {:rock :scissors
+              :paper :rock
+              :scissors :paper})
+
+(def losing (set/map-invert winning))
+
+(def score-for-play {:rock 1
+                     :paper 2
+                     :scissors 3})
 
 (defn read-input
   ([] (read-input "puzzle-2"))
   ([name]
    (->> (-> (slurp (str "resources/" name))
             (str/split #"\n"))
-        (map #(str/split %1 #" "))
-        (map (fn [[opp me]] {:opp (conversion opp) :me (conversion me)})))))
+        (map #(str/split %1 #" ")))))
 
 (defn score
   [{opp :opp, me :me}]
   (->
    (cond (= opp me) 3
-         (= me :rock) (if (= opp :scissors) 6 0)
-         (= me :scissors) (if (= opp :paper) 6 0)
-         (= me :paper) (if (= opp :rock) 6 0))
+         (= (winning me) opp) 6
+         :else 0)
    (+ (score-for-play me))))
+
+(defn move
+  [opp outcome]
+  (->
+   (case outcome
+     :draw opp
+     :win (losing opp)
+     :lose (winning opp))))
 
 (defn version-1
   []
   (->> (read-input)
+       (map (fn [[opp me]] {:opp (conversion-move opp) :me (conversion-move me)}))
+       (map #(score %1))
+       (reduce +)))
+
+(defn version-2
+  []
+  (->> (read-input)
+       (map (fn [[opp outcome]] {:opp (conversion-move opp) :outcome (conversion-outcome outcome)}))
+       (map (fn [{opp :opp outcome :outcome}] {:opp opp :me (move opp outcome)}))
        (map #(score %1))
        (reduce +)))
